0dc06b5d22929a76837c9fd1b144d89a6bc6c4b9
sahilkashyap64 <sahilkashyap64@gmail.com>
2026-06-02 23:23:34 +0530

    bottom nav and top nav in find player in mobile view added

--- src/pages/FindPlayersPage.tsx
+++ src/pages/FindPlayersPage.tsx
@@ -1528,7 +1528,7 @@ const FindPlayersPage = () => {
   })();
 
   return (
-    <MainLayout hideMobileNewMatch pageClassName="dashboard-page--find-players">
+    <MainLayout mobileChrome="home" desktopChrome="home" hideMobileNewMatch pageClassName="dashboard-page--find-players">
       <div className="find-players-page" style={themeVars}>
         <div className="find-players-page__inner">
           <ResultsHeader
